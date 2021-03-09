fc99b721997fbe2f4f20dafcbcb73b583df334b0
FletchyAF <joshua.f1998@gmail.com>
2021-03-09 09:09:45 -0500

    set up application_controller with helpers

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,40 @@
+class ApplicationController < Sinatra::Base
+
+    configure do
+        set(:views, 'app/views')
+        set :public_folder, 'public'
+        enable :sessions
+        set :session_secret, 'placeholder'
+    end
+
+    get '/' do
+        @name = name
+        erb :homepage
+    end
+
+    helpers do
+        
+        def current_user
+            @user = User.find_by(id: session[:id])
+        end
+
+        def redirect_if_not_logged_in
+            redirect to '/login' unless current_user
+        end
+
+        def set_brew
+            @brew = Brew.find_by(id: params[:id])
+        end
+
+        def check_brewmonger(obj)
+            obj && obj.user == current_user
+        end
+
+        def redirect_if_not_brewmonger
+            if !check_brewmonger(obj)
+                redirect '/brews'
+            end
+        end
+
+    end
+end
